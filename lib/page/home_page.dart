9abf7d99581a66b8e63e55d90bdfd4a3212d84c4
PUuMu <poomrapee.cps@gmail.com>
2024-07-05 06:44:25 +0700

    Select Json file
    Now you can select Json file

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:playtorium_test/data.dart';
 import '../function/price_calculator.dart';
 import '../datamodel/discount_model.dart';
+import '../datamodel/item_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,6 +22,8 @@ class _HomePageState extends State<HomePage> {
   bool _usepoints = false;
   List<DiscountModel> usedDiscounts = [];
   final TextEditingController pointsController = TextEditingController();
+  List<ItemModel> items = [];
+  List<DiscountModel> discounts = [];
 
   @override
   void dispose() {
@@ -53,6 +60,58 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Future<void> _loadDataFromJson() async {
+    try {
+      // Use FilePicker to select a file
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['json'],
+      );
+
+      if (result != null && result.files.single.path != null) {
+        File file = File(result.files.single.path!);
+        final contents = await file.readAsString();
+        final jsonData = jsonDecode(contents);
+
+        setState(() {
+          items = (jsonData['items'] as List)
+              .map((item) => ItemModel.fromJson(item))
+              .toList();
+          discounts = (jsonData['discounts'] as List)
+              .map((discount) => DiscountModel.fromJson(discount))
+              .toList();
+
+          // Recalculate prices after loading data
+          calculateTotalprice();
+          calculateFinalPrice();
+        });
+      } else {
+        // user canceled the picker
+        print('File selection canceled');
+      }
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error'),
+            content: const Text('cannot load data from this file'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+
+      print('Error loading data: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,9 +134,13 @@ class _HomePageState extends State<HomePage> {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Text('Cart',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    child: const Text('Cart',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold)),
+                  ),
                   const SizedBox(height: 10),
                   ...items.map((item) => Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -247,6 +310,13 @@ class _HomePageState extends State<HomePage> {
                       child: const Text('remove coupon')),
                 ],
               ),
+              const SizedBox(height: 40),
+              SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                    onPressed: _loadDataFromJson,
+                    child: const Text("Load Json Data")),
+              )
             ],
           ),
         ),
